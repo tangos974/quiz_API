66bfc146452a0e591f6fad4cdc215797ba0ad430
Ubuntu <ubuntu@ip-172-31-46-76.eu-west-1.compute.internal>
2023-11-10 02:11:11 +0000

    I broke it. FML.

--- launchapp.sh
+++ launchapp.sh
@@ -2,6 +2,9 @@ python3 -m pip install -r requirements.txt
 
 uvicorn app.main:app --reload --port 8000 &
 
-sleep 5
+sleep 3
 
 python3 ./app/dashapp.py
+
+pkill -f "uvicorn main:app"
+pkill -f "python dash_app.py"
